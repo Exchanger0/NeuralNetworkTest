6671008f2e117f18d5143477ff0362caa052e99c
Eldar <nazaroveldar531@gmail.com>
2024-09-15 16:03:14 +0500

    In nn2.py, the model is now a convolutional neural network (CNN)

--- nn2.py
+++ nn2.py
@@ -1,6 +1,7 @@
 import os
 os.environ["TF_CPP_MIN_LOG_LEVEL"] = "2"
 import keras as k
+from keras.api.layers import Dense, Conv2D, MaxPooling2D, Flatten
 import numpy as np
 from PIL import Image
 
@@ -14,18 +15,27 @@ y_test = np.array(y_test)
 x_train = x_train / 255
 x_test = x_test / 255
 
+x_train = np.expand_dims(x_train, axis=3)
+x_test = np.expand_dims(x_test, axis=3)
+
 y_train = k.utils.to_categorical(y_train, 10)
 y_test = k.utils.to_categorical(y_test, 10)
 
+print(x_train.shape)
+print(y_train.shape)
+
 model = k.Sequential([
-    k.layers.Input(shape=(28, 28, 1)),
-    k.layers.Flatten(input_shape=(28, 28, 1)),
-    k.layers.Dense(128, activation="sigmoid"),
-    k.layers.Dense(10, activation="softmax")
+    Conv2D(32, (3, 3), padding="same", activation="relu", input_shape=(28, 28, 1)),
+    MaxPooling2D((2, 2), 2),
+    Conv2D(64, (3, 3), padding="same", activation="relu"),
+    MaxPooling2D((2, 2), 2),
+    Flatten(),
+    Dense(128, activation="relu"),
+    Dense(10, activation="softmax")
 ])
 model.compile(loss="categorical_crossentropy", optimizer="adam", metrics=["accuracy"])
 print("Обучение нейронной сети началось")
-model.fit(x_train, y_train, batch_size=32, epochs=10)
+model.fit(x_train, y_train, batch_size=32, epochs=5, validation_split=0.2)
 print("Обучение нейронной сети закончилось")
 
 print("q для выхода")
